6ad2818ffa63e8f2fe56c8e8870ab548d592bbcb
Jake Fairbairn <jake_fairbairn@byu.net>
2017-10-06 17:55:01 -0600

    Updated jQuery files so collapse class would work on responsive nav bar.

--- 403_Project_1/Scripts/myScripts.js
+++ 403_Project_1/Scripts/myScripts.js
@@ -1,18 +1,25 @@
 ﻿function getNavBar()
 {
-    //Generated NavBar; located here to maintain consistency across pages
+    //Generated header NavBar; located here to maintain consistency across pages
     var html = '\
         <nav class="navbar navbar-default">\
-            <div class="container-fluid">\
-                <div class="navbar-header">\
-                    <a class="navbar-brand" href="#">WebSiteName</a>\
-                </div>\
+        <div class="container-fluid" >\
+            <div class="navbar-header">\
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#head_nav_content">\
+                    <span class="icon-bar"></span>\
+                    <span class="icon-bar"></span>\
+                    <span class="icon-bar"></span>\
+                </button>\
+                <a class="navbar-brand" href="#">WebSiteName</a>\
+            </div>\
+            <div class="collapse navbar-collapse" id="head_nav_content">\
                 <ul class="nav navbar-nav">\
                     <li><a href="Index">Home</a></li>\
                     <li><a href="Mission">Mission Page</a></li>\
                     <li><a href="About">About Us</a></li>\
                     <li><a href="Contact">Contact Us</a></li>\
                 </ul>\
+            </div>\
             </div >\
         </nav >\
     ';
@@ -22,6 +29,7 @@
 
 function getFootNavBar()
 {
+    //Generated footer NavBar; located here to maintain consistency across pages
     var html = '\
         <div class="container-fluid" >\
             <ul class="nav navbar-nav navbar-right">\
